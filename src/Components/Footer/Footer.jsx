d93674a45bc9bcdc6bd4bb02edf52cdcdf61e3dc
Bernard Ayamboro <bernardayam493@gmail.com>
2024-10-19 22:19:32 +0100

    footer build and minor bug fix

--- src/Components/Footer/Footer.jsx
+++ src/Components/Footer/Footer.jsx
@@ -1,9 +1,31 @@
-import React from 'react'
+import React from "react";
+import "./Footer.scss";
+import { Link } from "react-router-dom";
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
-}
+    <nav className="footer-section">
+      <section className="footer-container">
+        <div className="footer-container__logo">
+          <Link to="/">
+            <h1 className="footer-section__logo-container--text">
+              HAPPY PAWS!
+            </h1>
+          </Link>
+        </div>
+        <div className="footer-container__links">
+          <p className="footer-container__links-text">Overview</p>
+          <p className="footer-container__links-text">Features</p>
+          <p className="footer-container__links-text">Price</p>
+          <p className="footer-container__links-text">Help</p>
+          <p className="footer-container__links-text">Privacy</p>
+        </div>
+      </section>
+      <section className="footer-section__TCs__container">
+        <p className="footer-section__TCs__text">2024 Happy Paws. All rights reserved.</p>
+      </section>
+    </nav>
+  );
+};
 
-export default Footer
+export default Footer;
