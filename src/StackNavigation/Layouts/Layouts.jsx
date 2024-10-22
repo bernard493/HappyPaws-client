cf50f9fe177e94fc7727587e0f0a5c1236499bc1
Bernard Ayamboro <bernardayam493@gmail.com>
2024-10-22 14:16:25 +0100

    Hot-Fix get user profile and deleted unwanted codes

--- src/StackNavigation/Layouts/Layouts.jsx
+++ src/StackNavigation/Layouts/Layouts.jsx
@@ -1,12 +1,10 @@
-import React, { useEffect } from "react";
+import React from "react";
 import "./Layouts.scss";
 import { BrowserRouter as Router } from "react-router-dom";
 import Header from "../../Components/Header/Header";
 import Footer from "../../Components/Footer/Footer";
 import AppStack from "../AppStack/AppStack";
-import { useDispatch, useSelector } from "react-redux";
-import { GetUserProfile } from "../../API/User__Api";
-import { setUserGlobalState } from "../../store/globalStateSlice";
+
 
 const Layouts = () => {
 
