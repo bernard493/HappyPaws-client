520c1cc0552b039fa8b5df140748c7b46a2c77b9
Bernard Ayamboro <bernardayam493@gmail.com>
2024-11-02 01:06:45 +0000

    UI update search input

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -2,47 +2,19 @@ import React, { useState } from "react";
 import "./Hero.scss";
 import SearchInput from "../SearchInput/SearchInput";
 import { heroTexts } from "../../const/constant";
-import { useNavigate } from "react-router-dom";
 import Button from "../Button/Button";
 import TextAnimation from "../TextAnimation/TextAnimation";
 
 const Hero = () => {
-  const navigate = useNavigate();
-  const [searchInput, setSearchInput] = useState("");
   const [heroText] = useState(heroTexts);
-  const [isDisabled, setIsDisabled] = useState(false);
-  const [error, setError] = useState(false);
-
-  const handleSearchNavigation = () => {
-    // navigate to pets dashboard with searchInput
-    if (!searchInput) return setError(true);
-    setIsDisabled(true);
-    const queryParams = encodeURIComponent(searchInput).toString();
-    navigate(`/pet-matches?search=${queryParams}`);
-    setIsDisabled(false);
-  };
 
   return (
     <div className="Hero">
       <section className="Hero__text--container">
         <h1 className="Hero__title">I am Pawsist, Your AI Assistance</h1>
-        {/* <h2 className="Hero__text">{heroText[generateRandomIndex]}</h2> */}
-        <TextAnimation text={heroText} />
+        {/* <TextAnimation text={heroText} /> */}
         <div className="Hero__input__section--container">
-          <div>
-            <SearchInput
-              isError={error}
-              setIsError={setError}
-              setValue={setSearchInput}
-              placeholder="I'm Looking for a pet that’s great with..."
-            />
-          </div>
-          <Button
-            handleButtonClick={handleSearchNavigation}
-            isDisabledState={isDisabled}
-            notDisabledText={"Meet Your Pet Pall"}
-            isDisabledText={"Finding Pet Pall..."}
-          />
+          <SearchInput placeholder="How can PawSist help ypu todayF" />
         </div>
       </section>
       <section className="Hero__blog--container">
--- src/Components/SearchInput/SearchInput.jsx
+++ src/Components/SearchInput/SearchInput.jsx
@@ -1,26 +1,49 @@
-import React from "react";
+import React, { useState } from "react";
 import "./SearchInput.scss";
 import { SiMinds } from "react-icons/si";
+import { FaArrowRight } from "react-icons/fa6";
+import Button from "../Button/Button";
+import { useNavigate } from "react-router-dom";
 
-const SearchInput = ({ placeholder, setValue, isError, setIsError }) => {
+const SearchInput = ({ placeholder,  setIsError }) => {
+  const navigate = useNavigate();
+  const [isDisabled, setIsDisabled] = useState(false);
+  const [searchInput, setSearchInput] = useState("");
+  const [error, setError] = useState(false);
+
+
+  const handleSearchNavigation = () => {
+    // navigate to pets dashboard with searchInput
+    if (!searchInput) return setError(true);
+    setIsDisabled(true);
+    const queryParams = encodeURIComponent(searchInput).toString();
+    navigate(`/pet-matches?search=${queryParams}`);
+    setIsDisabled(false);
+  };
+
   return (
-    <>
-      <div className={`searchInput__container ${isError && "error__border"}`}>
-        <SiMinds />
-        <input
-          onChange={(e) => setValue(e.target.value)}
+    <div className="searchInput__container">
+      <div className="searchInput__container__text--container">
+        <textarea
+          onChange={(e) => setSearchInput(e.target.value)}
           className="input"
           type="text"
           placeholder={placeholder}
           onFocus={() => setIsError(false)}
+          value={searchInput}
         />
+       <Button
+            handleButtonClick={handleSearchNavigation}
+            isDisabledState={isDisabled}
+            notDisabledText={<FaArrowRight/>}
+            isDisabledText={<FaArrowRight/>}
+            width={20}
+          /> 
       </div>
-      {isError && (
-        <p className="error">
-          Please provide details to find the best matches.
-        </p>
-      )}
-    </>
+      <div>
+        <SiMinds color="white"/>
+      </div>
+    </div>
   );
 };
 
